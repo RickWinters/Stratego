61f0ec8e5a278d6c525d1e44e477bb690302e0d2
RickWinters <rickwinters12@hotmail.com>
2019-04-23 11:37:18 +0200

    test

---
+++ aantekeningen.java
@@ -0,0 +1,21 @@
+public class aantekeningen {
+    public static void main(String[] args) {
+        /*
+        UML diagrammen.
+
+
+        SCM (Source Code management)
+        Git, een programma om een repository op te starten en te managene
+        een repository is een plek voor source code (dropbox voor developers)
+        Github is een programma waarmee een gitrepository gedeeld kan worden met een server
+        command 'push' betekend een repository delen met de server.
+        Command 'pull' is een repository ophalen
+
+
+        In de locale repository kan men iets veranderen
+        met 'git add' komen ze in een staging area
+        met 'git commit -am' worden de files gepushed naar de off-site repository. Hiermee kan men een berichtje / notitie erbij voeren
+
+         */
+    }
+}
