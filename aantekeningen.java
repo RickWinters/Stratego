cb8b470e2de58fe72686c84b09dee2ec6298792f
RickWinters <rickwinters12@hotmail.com>
2019-04-24 11:49:46 +0200

    test

--- aantekeningen.java
+++ aantekeningen.java
@@ -34,6 +34,37 @@ public class aantekeningen {
         SELECT id, price from CAR where id = 2"
         Select (de kolommon) van (tabel) where (velden)
 
+
+        A database is a set of interconnected relations (tables)
+        RDMs Relational Datable Management System
+        One to Many reference. Een persoon kan meerdere fietsen hebben, maar een fiets heeft altijd maar 1 eigenaar.
+
+        Linked Table
+        In de hoofdtable staan de ID van de afzonderlijke 'many to one' tabellen
+
+            has many    >    users_projects   <          has many
+            /\  has one   <     ||            > has one
+           | |    \/            ||                 \/        /\
+          Users                 ||               Projects
+          users aan nummers     ||               en een nummer aan projects
+          te koppelen           ||
+                                kunnen hier een tabel van user-ids
+                                en projects id gemaakt worden
+
+         we gaan mySQL
+         lifecycle van een project / tabel is CRUD
+         Create
+         Read
+         Update
+         Delete
+
+         syntax:
+         SELECT <field names seperated by commas> FROM <table name> WHERE <key> = <value>   Read
+         UPDATE <table_name> SET <key> = <newvalue> WHERE <key> = <value>                   Update   veld veranderen
+         INSERT <table_name> (<column_name_1, ...) VALUES <value_1, ..)                     Update   rij toevoegen, het aantal values in beide dingen moeten overeenkomen. Een row_id wordt meegegeven automatisch.
+         DELETE FROM <table_name> WHERE <key> = <value>                                     Verwijder rij uit tabel
+
          */
+
     }
 }
